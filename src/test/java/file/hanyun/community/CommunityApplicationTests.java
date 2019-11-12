f7fa21cbf0f807035ff8013ccbbdb200c29168f9
hanyun.li <739125492@qq.com>
2019-11-12 18:04:39 +0800

    init repo

---
+++ src/test/java/file/hanyun/community/CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package file.hanyun.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
